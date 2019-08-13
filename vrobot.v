6722bd7858547c3dc54ad6fd28d12d6d3e54b380
Kalle <khirvola@gmail.com>
2019-08-13 23:10:36 +0300

    Added drag() and drag_rel() Added seperate mouse_up() and mouse_down() functions

--- vrobot.v
+++ vrobot.v
@@ -5,31 +5,61 @@ import time
 
 #include "windows.h"
 
+const (
+  MOUSE_STEPS = 500
+)
+
 struct Point {
   x int
   y int
 }
 
-pub fn click(button string) {
+fn do_mouse_event(x int) {
+  C.mouse_event(x, 0, 0, 0, 0)
+}
+
+fn mouse_down(button string) {
   match button {
-    'left' => {
-      C.mouse_event(C.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-      C.mouse_event(C.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
-    }
-    'right' => {
-      C.mouse_event(C.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
-      C.mouse_event(C.MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
-    }
-    'middle' => {
-      C.mouse_event(C.MOUSEEVENTF_MIDDLEDOWN, 0, 0, 0, 0)
-      C.mouse_event(C.MOUSEEVENTF_MIDDLEUP, 0, 0, 0, 0)
-    }
+    'left' => do_mouse_event(C.MOUSEEVENTF_LEFTDOWN)
+    'right' => do_mouse_event(C.MOUSEEVENTF_RIGHTDOWN)
+    'middle' => do_mouse_event(C.MOUSEEVENTF_MIDDLEDOWN)
+    else => panic('Invalid mouse button')
   }
+}
+
+fn mouse_up(button string) {
+  match button {
+    'left' => do_mouse_event(C.MOUSEEVENTF_LEFTUP)
+    'right' => do_mouse_event(C.MOUSEEVENTF_RIGHTUP)
+    'middle' => do_mouse_event(C.MOUSEEVENTF_MIDDLEUP)
+    else => panic('Invalid mouse button')
+  }
+}
+
+pub fn click(button string) {
+  mouse_down(button)
+  mouse_up(button)
 }
 
 pub fn double_click(button string) {
   click(button)
   click(button)
+}
+
+pub fn drag(x, y int) {
+  mouse_down('left')
+  move_mouse(x, y)
+  mouse_up('left')
+}
+
+pub fn drag_rel(offset_x, offset_y int) {
+  start_pos := get_mouse_pos()
+  dest_x := start_pos.x + offset_x
+  dest_y := start_pos.y + offset_y
+
+  mouse_down('left')
+  move_mouse(dest_x, dest_y)
+  mouse_up('left')
 }
 
 pub fn move_mouse(x, y int) {
@@ -49,16 +79,15 @@ pub fn move_mouse_rel(offset_x, offset_y, duration_ms int) {
 }
 
 pub fn move_mouse_smooth(dest_x, dest_y, duration_ms int) {
-  steps := 500
   start_pos := get_mouse_pos()
 
-  dx := (f32(dest_x) - start_pos.x) / steps
-  dy := (f32(dest_y) - start_pos.y) / steps
-  dt := int(f32(duration_ms) / f32(steps))
+  dx := (f32(dest_x) - start_pos.x) / MOUSE_STEPS
+  dy := (f32(dest_y) - start_pos.y) / MOUSE_STEPS
+  dt := int(f32(duration_ms) / f32(MOUSE_STEPS))
 
   mut i := 0
   
-  for i < steps {
+  for i < MOUSE_STEPS {
     x := int(f32(start_pos.x) + dx * i)
     y := int(f32(start_pos.y) + dy * i)
 
@@ -80,7 +109,12 @@ pub fn get_mouse_pos() Point {
 }
 
 fn main() {
+  $if !windows {
+    panic('You need windows machine')
+  }
+
   // Test code
-  move_mouse_rel(100, 100, 500)
-  double_click('left')
+  p := get_mouse_pos()
+  println('Mouse position: $p.x, $p.y')
+  drag_rel(50, 234)
 }
