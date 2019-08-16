c5f52e68a50ef2689d214d9350a32b75bf33460f
Kalle <khirvola@gmail.com>
2019-08-16 21:40:19 +0300

    test

--- vrobot.v
+++ vrobot.v
@@ -5,6 +5,8 @@ import time
 
 #include "windows.h"
 
+struct C.INPUT
+
 struct Point {
 pub:
   x int
@@ -146,3 +148,37 @@ pub fn get_mouse_pos() Point {
 
   return p
 }
+
+struct C.COLORREF
+
+pub fn get_pixel_color(x, y int) /* C.COLORREF */ {
+  hdc := C.GetDC(C.NULL)
+
+  if !hdc {
+    panic('Could not get device context handle')
+  }
+
+  mut c := i32(0)
+  c = C.GetPixel(hdc, x, y)
+  C.ReleaseDC(C.GetDesktopWindow(), hdc)
+
+  // return color
+}
+
+/*
+pub fn type_stuff() {
+  // Keyboard
+  input := &C.INPUT{}
+  vkey := C.VK_RETURN
+  input.type = 1 // C.INPUT_KEYBOARD
+  input.ki.wScan = C.MapVirtualKey(vkey, C.MAPVK_VK_TO_VSC)
+  input.ki.time = 0 
+  input.ki.dwExtraInfo = 0
+  input.ki.wVk = vkey
+  input.ki.dwFlags = 0
+  C.SendInput(1, &input, sizeof(C.INPUT))
+
+  input.ki.dwFlags = C.KEYEVENTF_KEYUP
+  C.SendInput(1, &input, sizeof(C.INPUT))
+}
+*/
